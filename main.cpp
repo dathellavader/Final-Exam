a15a23b5d55c2b178544c0f1f395d04c383fb9d2
dathellavader <ella.henner@pcc.edu>
2025-12-02 21:14:21 +0000

    work

---
+++ main.cpp
@@ -0,0 +1,97 @@
+// ------------- FILE HEADER -------------
+// Author ✅: Ella Henner
+// Assignment ✅: Final Exam
+// Date ✅: 12/7/25
+// Citations: 
+
+
+// ------------- ZYBOOKS SCORES -------------
+// Chapter ✅: 
+// Participation ✅: 
+// Challenge ✅:
+// Labs ✅:
+
+
+// ------------- DISCORD POSTS -------------
+// https://discord.com/invite/URYKKf8YHm
+// Count ✅:
+// Links (Optional): 
+
+
+// ------------- DESIGN DOCUMENT -------------
+// A. INPUT ✅: 
+// B. OUTPUT ✅:
+// C. CALCULATIONS ✅:
+// D. LOGIC and ALGORITHMS ✅:
+//    (Optional) flow chart link or file name: 
+
+
+// ------------- TESTING -------------
+// PASS ALL GIVEN SAMPLE RUN TESTS ✅: 
+// (Optional) Additional tests count:   
+
+
+// ------------- CODE -------------
+#include <iostream>
+
+using namespace std;
+
+// funct prototypes
+void getInput(int &goal);
+int calcTotal();
+
+// main
+int main() {
+
+cout << "Welcome to my Miles Tracker program." << endl;
+
+getInput(int &goal)
+
+
+
+  return 0;
+}
+
+// getInput funct
+double goal;
+cout << "How many miles do you want to ride this week?";
+cin >> goal;
+
+
+
+
+
+
+
+
+
+
+// ------------- DESIGN -------------
+/* 
+Program Name:
+
+Program Description:
+
+Design:
+A. INPUT
+Define the input variables including name data type. 
+
+B. OUTPUT
+Define the output variables including data types. 
+
+C. CALCULATIONS
+Describe calculations used by algorithms in step D.  
+List all formulas. 
+If there are no calculations needed, state there are no calculations.
+
+D. LOGIC and ALGORITHMS
+Design the logic of your program using pseudocode or flowcharts. 
+Use conditionals, loops, functions or array constructs.
+List the steps in transforming inputs into outputs. 
+https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
+
+
+SAMPLE RUNS
+Copy from assignment document.
+
+*/
